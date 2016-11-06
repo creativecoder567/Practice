c6a620dcae76e090e7b2ff929f3d3690d80e401b
creativecoder567 <sarathcreativethinker@gmail.com>
2016-11-06 13:09:23 +0530

    test

---
+++ app/src/main/java/com/sarath/practice/windowsButtton.java
@@ -0,0 +1,19 @@
+package com.sarath.practice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sarath on 11/6/2016.
+ */
+
+public class windowsButtton extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.windows);
+        ///fghgfghfghfh
+
+    }
+}
